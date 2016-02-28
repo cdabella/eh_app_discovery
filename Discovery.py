9c89dd7bb01f2d37752a7b7bc9c4a40192f9c7e0
cdabella <abella.cd@gmail.com>
2016-02-27 16:11:41 -0800

    Implemented device metric collecting

--- Discovery.py
+++ Discovery.py
@@ -3,24 +3,63 @@ class Discoverer(object):
 
     import httplib
 
-    def __init__(self, apikey='', host=''):
-        self.apikey = apikey
-        self.host   = host
+    def __init__(self, apikey='', host='', lookback=-86400000):
+        self.apikey   = apikey
+        self.host     = host
+        self.lookback = lookback
 
     def get_devices_by_type(self, type):
         return _eh_api_get('/api/v1/devices?' +
                            'limit=0&' +
                            'search_type=type&' +
                            'value='+ type + '&' +
-                           'active_from=-86400000&' +
+                           'active_from=' + str(self.lookback) + '&' +
                            'active_until=0')
+
+    def get_device_metrics(self,device_id, metric_category, metric):
+
+    body = {
+                "cycle": "auto",
+                "from": self.lookback,
+                "metric_category": metric_category.split('.')[2],
+                "metric_specs": [{"name": metric}],
+                "object_ids": [device_id],
+                "object_type": "device",
+                "until": 0
+            }
+
+    response = _eh_api_get('/api/v1/metrics/total', str(body))
+    return response.stats.values[0]
 
     def tag_by_metric_identifier(self, metric_category, metric, key, tag):
 
         # Check that tag exists
         tag_id      = _make_tag(tag)
+
+        # Determine the device type required for the metric category
         device_type = _process_metric_category(metric_category)
-        devices     = get_devices_by_type(device_type) 
+
+        # Get a list of all devices of that device type
+        devices     = get_devices_by_type(device_type)
+
+        # Populate device list with the metric
+        for i in range(0, len(devices)):
+            device_id = devices[i]['id']
+            devices[i]['device_metrics'] = get_device_metrics(device_id, metric_category, metric)
+
+        # From this point, functionality can diverge.
+        # A) Find known identifiers and tag accordingly.
+        # B) Group all detail metrics and determine possible identifiers
+        # This function will implement (A)
+
+        for device in devices:
+            if !(type(device['device_metrics']) is list):
+                raise ValueError('Expecting list (Topnset)', device['device_metrics'])
+
+            for metric in device['device_metrics']:
+                if (key in metric.key.str):
+                    
+
 
     def _make_tag(self, tag):
         # Check that tag exist
@@ -54,7 +93,7 @@ class Discoverer(object):
                 return mc[i-1] + '_' + mc[i]
 
 
-    def _eh_api_get(self, path):
+    def _eh_api_get(self, path, body=''):
         headers = {'Accept': 'application/json',
                    'Authorization': 'ExtraHop apikey=' + self.apikey}
         conn = httplib.HTTPSConnection(self.host)
@@ -65,7 +104,7 @@ class Discoverer(object):
         else:
             return resp.read()
 
-    def _eh_api_post(self, path, body):
+    def _eh_api_post(self, path, body=''):
         headers = {'Accept': 'application/json',
                    'Authorization': 'ExtraHop apikey=' + self.apikey}
         conn = httplib.HTTPSConnection(self.host)
